bc08600b793cb1f1a982be691f79b97aee00996a
eggplants <w10776e8w@yahoo.co.jp>
2022-06-05 10:53:21 +0900

    feat: packaging with setup.cfg

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from setuptools import setup, find_packages
-from namedivider import __version__
+from setuptools import setup
 
-setup(
-    name='namedivider-python',
-    version=__version__,
-    url='https://github.com/rskmoi/namedivider-python',
-    author="rskmoi",
-    author_email='rei.sakamoto.92@gmail.com',
-    description='A tool for dividing the Japanese full name into a family name and a given name.',
-    license="MIT",
-    packages=find_packages(),
-    install_requires=['numpy', 'pandas', 'tqdm', 'click', 'regex', 'typer>=0.3.2'],
-    tests_require=["pytest"],
-    entry_points={
-        'console_scripts': ['nmdiv = namedivider.cli:app']
-    },
-    package_data={'': ['namedivider/assets/*.csv']},
-    include_package_data=True,
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
-    ]
-)
+setup()
