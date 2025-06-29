01cdf6001ce426f60edccb49e6cc84a40cae2f83
rskmoi <rei.sakamoto.92@gmail.com>
2025-06-29 13:48:13 +0900

    use namedivider-core v0.2.2

--- scripts/benchmark_sample.sh
+++ scripts/benchmark_sample.sh
@@ -6,9 +6,9 @@
 hyperfine \
     --warmup 2 \
     --runs 10 \
-    "nmdiv benchmark test_names_sample.txt --mode basic --no-mask-cache --silent" \
-    "nmdiv benchmark test_names_sample.txt --mode basic --use-mask-cache --silent" \
-    "nmdiv benchmark test_names_sample.txt --mode basic --no-mask-cache --backend rust --silent" \
-    "nmdiv benchmark test_names_sample.txt --mode gbdt --no-mask-cache --silent" \
-    "nmdiv benchmark test_names_sample.txt --mode gbdt --use-mask-cache --silent" \
-    "nmdiv benchmark test_names_sample.txt --mode gbdt --no-mask-cache --backend rust --silent" \
+    "python -m namedivider.cli benchmark test_names_sample.txt --mode basic --no-mask-cache --silent" \
+    "python -m namedivider.cli benchmark test_names_sample.txt --mode basic --use-mask-cache --silent" \
+    "python -m namedivider.cli benchmark test_names_sample.txt --mode basic --no-mask-cache --backend rust --silent" \
+    "python -m namedivider.cli benchmark test_names_sample.txt --mode gbdt --no-mask-cache --silent" \
+    "python -m namedivider.cli benchmark test_names_sample.txt --mode gbdt --use-mask-cache --silent" \
+    "python -m namedivider.cli benchmark test_names_sample.txt --mode gbdt --no-mask-cache --backend rust --silent" \
